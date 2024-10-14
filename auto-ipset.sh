445009cfc2d423f3ee0ff13ea083d4c6a72ca2ed
Lan Tian <xuyh0120@outlook.com>
2024-10-13 23:52:48 -0700

    Auto setup firewall rule to block bad IPs

--- auto-ipset.sh
+++ auto-ipset.sh
@@ -3,6 +3,9 @@
 IPSET=$1
 URL=$2
 
+ipset create "${IPSET}_v4" || true
+ipset create "${IPSET}_v6" || true
+
 while true; do
     TMP_FILE="/tmp/ipset_${IPSET}.txt"
     curl -fsSL "$URL" -o "$TMP_FILE" || {
@@ -13,8 +16,7 @@ while true; do
 
     {
         IPV4=$(grep -E "^[0-9\.]+$" "$TMP_FILE")
-        echo "destroy ${IPSET}_v4"
-        echo "create ${IPSET}_v4 hash:net family inet hashsize 1024 maxelem 1038576"
+        echo "flush ${IPSET}_v4"
         for IP in $IPV4; do
             echo "add ${IPSET}_v4 ${IP}"
         done
@@ -22,8 +24,7 @@ while true; do
 
     {
         IPV6=$(grep -E "^[0-9a-f:\\.]*:[0-9a-f:\\.]*$" "$TMP_FILE")
-        echo "destroy ${IPSET}_v6"
-        echo "create ${IPSET}_v6 hash:net family inet6 hashsize 1024 maxelem 1038576"
+        echo "flush ${IPSET}_v6"
         for IP in $IPV6; do
             echo "add ${IPSET}_v6 ${IP}"
         done
